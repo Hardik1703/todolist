5cf32b382d09f27864ab05c04f662ce7f0a9b439
Hardik1703 <gambhavahardik1703@gmail.com>
2023-10-29 12:41:38 +0530

    Form add

--- app/page.js
+++ app/page.js
@@ -3,7 +3,12 @@ import React from 'react'
 const page = () => {
   return (
     <>
-      <h1>TodoList</h1>
+      <h1 className='bg-gray-500 text-6xl text-white text-center p-5'>TodoList</h1>
+      <form>
+        <input type="text" placeholder='Enter Title' className='border border-3 border-black m-5 p-3 rounded' />
+        <input type="text" placeholder='Enter Details' className='border border-3 border-black m-5 p-3 rounded' />
+        <button className='bg-black text-white py-3 px-4 rounded'>Submit</button>
+      </form>
     </>
   )
 }
